be7d876983b664c0a44596fbb6ae2a15f0ebd4a7
Thomas Ferré <t.ferre@digitsole.com>
2022-09-20 18:04:02 +0200

    [#View] Search View
    - Outlets added
    - DismissKeyboard method added
    - AddIngredient method added
    - Complience to UITableViewDataSource, UITextfieldDelegate and UITableViewDelegate for SearchViewController

--- Reciplease/Reciplease/Controller/SearchViewController.swift
+++ Reciplease/Reciplease/Controller/SearchViewController.swift
@@ -9,21 +9,63 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var ingredientsTextField: UITextField!
+    @IBOutlet weak var addIngredientButton: UIButton!
+    @IBOutlet weak var ingredientTableView: UITableView!
+    @IBOutlet weak var clearIngredientListButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        ingredientsTextField.resignFirstResponder()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func addIngredient(_ sender: Any) {
+        guard let name = ingredientsTextField.text else { return }
+        let ingredient = Ingredient(name: name)
+        IngredientService.shared.add(ingredient: ingredient)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.ingredientTableView.reloadData()
+            self.ingredientsTextField.text = ""
+        }
     }
-    */
+}
 
+extension SearchViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return IngredientService.shared.ingredients.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientCell", for: indexPath)
+        let ingredient = IngredientService.shared.ingredients[indexPath.row]
+        cell.textLabel?.text = "- \(ingredient.name)"
+        cell.textLabel?.textColor = UIColor.white
+        return cell
+    }
+}
+
+extension SearchViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+       if editingStyle == .delete {
+           IngredientService.shared.remove(at: indexPath.row)
+           tableView.deleteRows(at: [indexPath], with: .automatic)
+       }
+    }
 }
